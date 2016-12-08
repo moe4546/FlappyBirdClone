92356672e5d242f0147e5f879bfcf25500dd9f70
Moritz <horn.moritz@web.de>
2016-12-08 12:13:40 +0100

    no message

---
+++ Assets/Scripts/Obstacles.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacles : MonoBehaviour {
+
+	private Rigidbody2D rigi;
+	public Vector2 velocity = new Vector2(-4f, 0f);
+
+	void Start() {
+		rigi = GetComponent<Rigidbody2D> ();
+		rigi.velocity = velocity;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D gravity;
+	public Vector2 jumpForce = new Vector2 (0, 300f);
+
+	void Start() {
+		gravity = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update() {
+		if(Input.GetMouseButton(0)) {
+			gravity.velocity = Vector2.zero;
+			gravity.AddForce (jumpForce);
+		}
+	}
+
+	void OnCollisionEnter2D() {
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
